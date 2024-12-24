98dec6443078bcccd022f0282412f920c1f7e11c
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2024-12-24 17:20:28 -0300

    chore: permit iam user can assume iam role

--- iac/iam.tf
+++ iac/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_user" "amazon_sales_iam_user" {
-  name = "amazon-sales-role"
+  name = "amazon-sales-iam-user"
 }
 
 resource "aws_iam_access_key" "amazon_sales_iam_access_key" {
@@ -17,6 +17,29 @@ resource "aws_iam_role" "amazon_sales_iam_role" {
         "Principal" : {
           "Service" : "ec2.amazonaws.com"
         }
+      },
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : aws_iam_user.amazon_sales_iam_user.arn
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy" "assume_role_policy" {
+  name = "assume-role-policy"
+  user = aws_iam_user.amazon_sales_iam_user.name
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRole",
+        "Resource" : aws_iam_user.amazon_sales_iam_user.arn
       }
     ]
   })
