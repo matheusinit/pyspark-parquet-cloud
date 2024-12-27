1cd74e8ea5c5f765f9ed24bd579a8c6ca5a29c14
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2024-12-26 22:02:19 -0300

    style: format terraform code

--- iac/main.tf
+++ iac/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.62"
     }
   }
@@ -10,6 +10,6 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region  = "us-west-2"
   profile = "amazon-sales"
 }
