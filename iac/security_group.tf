b5087379ead493cf92169bb471f416c8eb73d81a
Matheus Oliveira <matheus.oliveira.s@protonmail.com>
2024-12-28 13:45:53 -0300

    fix: break security group rules in unique units to ensure none dependency cycle between security groups

--- iac/security_group.tf
+++ iac/security_group.tf
@@ -7,26 +7,14 @@ resource "aws_security_group" "emr_master_security_group" {
   description = "Security Group for EMR Master Cluster"
   vpc_id      = aws_vpc.amazon_sales_vpc.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
-  }
-
-  ingress {
-    from_port   = 443 # Allow HTTPS connections (for console access)
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allow outbound traffic to anywhere
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  revoke_rules_on_delete = true
 }
 
 resource "aws_security_group" "emr_slave_security_group" {
@@ -34,26 +22,14 @@ resource "aws_security_group" "emr_slave_security_group" {
   description = "Security Group for EMR Slave Cluster"
   vpc_id      = aws_vpc.amazon_sales_vpc.id
 
-  ingress {
-    from_port   = 22 # Allow SSH connections
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
-  }
-
-  ingress {
-    from_port   = 443 # Allow HTTPS connections (for console access)
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allow outbound traffic to anywhere
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  revoke_rules_on_delete = true
 }
 
 resource "aws_security_group_rule" "inbound_rule_for_emr_master" {
@@ -61,10 +37,8 @@ resource "aws_security_group_rule" "inbound_rule_for_emr_master" {
   from_port                = 0
   to_port                  = 0
   protocol                 = "-1"
-  source_security_group_id = aws_security_group.emr_slave_security_group.id
   security_group_id        = aws_security_group.emr_master_security_group.id
-
-  depends_on = [aws_security_group.emr_slave_security_group]
+  source_security_group_id = aws_security_group.emr_slave_security_group.id
 }
 
 resource "aws_security_group_rule" "inbound_rule_for_emr_slave" {
@@ -72,8 +46,15 @@ resource "aws_security_group_rule" "inbound_rule_for_emr_slave" {
   from_port                = 0
   to_port                  = 0
   protocol                 = "-1"
-  source_security_group_id = aws_security_group.emr_master_security_group.id
   security_group_id        = aws_security_group.emr_slave_security_group.id
+  source_security_group_id = aws_security_group.emr_master_security_group.id
+}
 
-  depends_on = [aws_security_group.emr_master_security_group]
+resource "aws_security_group_rule" "inbound_ssh_rule_for_emr_master" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.emr_master_security_group.id
+  cidr_blocks       = ["${chomp(data.http.myip.response_body)}/32"]
 }
